b14f56bc9ab4de3517758cd39fc6373f48bc1c0f
Matty <54020396+matty-ross@users.noreply.github.com>
2026-04-10 20:13:38 +0200

    Implemented logging into the console.

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,3 +1,7 @@
+#include <cstdarg>
+#include <cstdio>
+#include <Windows.h>
+
 #include "Core/Logger.hpp"
 
 
@@ -8,9 +12,31 @@ namespace Core
         m_Name(name)
     {
     }
-    
+
     void Logger::Log(const char* level, const char* format, ...) const
     {
-        // TODO: Log into the console.
+        SYSTEMTIME localTime = {};
+        GetLocalTime(&localTime);
+
+        char message[1024] = {};
+
+        va_list args = {};
+        va_start(args, format);
+        vsprintf_s(message, format, args);
+        va_end(args);
+
+        printf_s(
+            "[%04d-%02d-%02d %02d:%02d:%02d.%03d]  %-10s  %-20s  :  %s\n",
+            localTime.wYear,
+            localTime.wMonth,
+            localTime.wDay,
+            localTime.wHour,
+            localTime.wMinute,
+            localTime.wSecond,
+            localTime.wMilliseconds,
+            level,
+            m_Name,
+            message
+        );
     }
 }
